1c3fdfda6245cabdb930a6c6fc471fd1133af0eb
nabab <thomas.nabet@gmail.com>
2018-03-18 03:32:59 +0100

    removed locales

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -6,13 +6,7 @@
 
 /** @var $this \bbn\mvc\controller */
 
-//$ctrl->data['root'] = $ctrl->plugin_url('appui-server').'/';
 if ( !\defined('APPUI_SERVER_ROOT') ){
   define('APPUI_SERVER_ROOT', $ctrl->plugin_url('appui-server').'/');
 }
-//bindtextdomain('appui_task', BBN_LIB_PATH.'bbn/appui-task/src/locale');
-//setlocale(LC_ALL, "fr_FR.utf8");
-
-textdomain('appui_server');
-
 return 1;
