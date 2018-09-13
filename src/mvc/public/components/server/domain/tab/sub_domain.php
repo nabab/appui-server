cbd196645f77f2502173e7da4f5f37956c5675f4
nabab <thomas.nabet@gmail.com>
2018-09-13 05:07:18 +0200

    Moved components

--- src/mvc/public/components/server/domain/tab/sub_domain.php
+++
@@ -1,3 +0,0 @@
-<?php
-  $ctrl->combo();
-  $ctrl->obj->url = APPUI_SERVER_ROOT.'ui/server/'.$ctrl->arguments[0]. '/domains/'.$ctrl->arguments[2].'/sub_domains/'.$ctrl->arguments[4];
--- src/mvc/public/components/server/widget/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by BBN Solutions.
- * User: Vito Fava
- * Date: 04/06/18
- * Time: 16.24
- */
-
-$ctrl->combo();
