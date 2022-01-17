72977cbab5c04e7c44a73ae0c39fc010c5c1a721
Mirko Argentino <mirko@bbn.solutions>
2022-01-17 10:27:19 +0100

    Fixed poller model

--- src/plugins/appui-core/model/poller.php
+++ src/plugins/appui-core/model/poller.php
@@ -7,24 +7,30 @@ return [[
       'success' => true,
       'data' => []
     ];
-    $running = \bbn\Appui\Server::getRunningTasks();
-    $runningHash = md5(json_encode($running));
-    if ($runningHash !== $data['data']['runningTasksHash']) {
-      $res['data'] = [
-        'runningTasks' => $running,
-        'serviceWorkers' => [
-          'runningTasksHash' => $runningHash
-        ]
-      ];
-    }
-    $queue = \bbn\Appui\Server::getCurrentTasksQueue();
-    $queueHash = md5(json_encode($queue));
-    if ($queueHash !== $data['data']['tasksQueueHash']) {
-      $res['data']['tasksQueue'] = $queue;
-      if (!isset($res['data']['serviceWorkers'])) {
-        $res['data']['serviceWorkers'] = [];
+    if (isset($data['data'])) {
+      $running = \bbn\Appui\Server::getRunningTasks();
+      $runningHash = md5(json_encode($running));
+      if (isset($data['data']['runningTasksHash'])
+        && ($runningHash !== $data['data']['runningTasksHash'])
+      ) {
+        $res['data'] = [
+          'runningTasks' => $running,
+          'serviceWorkers' => [
+            'runningTasksHash' => $runningHash
+          ]
+        ];
       }
-      $res['data']['serviceWorkers']['tasksQueueHash'] = $queueHash;
+      $queue = \bbn\Appui\Server::getCurrentTasksQueue();
+      $queueHash = md5(json_encode($queue));
+      if (isset($data['data']['tasksQueueHash'])
+        && ($queueHash !== $data['data']['tasksQueueHash'])
+      ) {
+        $res['data']['tasksQueue'] = $queue;
+        if (!isset($res['data']['serviceWorkers'])) {
+          $res['data']['serviceWorkers'] = [];
+        }
+        $res['data']['serviceWorkers']['tasksQueueHash'] = $queueHash;
+      }
     }
     return $res;
   }
